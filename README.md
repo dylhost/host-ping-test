87ed816c1bb76fc85830edddcf70a2733ab85c82
root <root@syd.plex.re>
2024-12-09 18:07:58 +1100

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+Credits for original template code https://github.com/johnwmintz/pinglist
+
+# Features
+Outputs total & Average (good for determining how well your server is peered internationally)
+
+# Future feature
+Outputs min / max / avg / total (good for comparing global peering of your box)
+
+Outputs min / max provider
+
+
+Sort results here:
+https://miniwebtool.com/sort-numbers/
---
+++ pinglist.sh
@@ -0,0 +1,30 @@
+total=0
+count=0
+min=999
+max=0
+mintxt=""
+maxtxt=""
+while read output
+do
+    ping=$(ping -4 -qc1 $(echo $output | cut -d "#" -f 1) 2>&1 | awk -F'/' 'END{ print (/^rtt/? $5:"FAIL") }')
+    echo $ping" ms" $output
+    total=$(echo "$total+$ping" | bc)
+    count=$(echo "$count+1" | bc)
+    if (( $(echo "$ping" != "FAIL" | bc -l) ))
+    then
+        if (( $(echo "$ping < $min" | bc -l) ))
+        then
+            min=$ping
+            mintxt="$output"
+        fi
+        if (( $(echo "$ping > $max" | bc -l) ))
+        then
+            max=$ping
+            maxtxt="$output"
+        fi
+    fi
+done < <(cat list.txt| tail -n +4)
+
+echo "min/avg/max/total" $min"/"$(echo "$total/$count" | bc)""/""$max"/""$total"
+echo "min provider: "$mintxt "("$min"ms)"
+echo "max provider: "$maxtxt "("$max"ms)"
