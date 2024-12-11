47cd50c8654c6e9f553b3a4fa852e2279361c287
dylhost <admin@dylhost.tech>
2024-12-11 19:33:43 +1100

    Added progress bar as list is getting lengthy....

--- pinglist.sh
+++ pinglist.sh
@@ -1,14 +1,43 @@
+#!/bin/bash
+bar_size=40
+bar_char_done="#"
+bar_char_todo="-"
+bar_percentage_scale=2
+
+function show_progress {
+    current="$1"
+    total="$2"
+
+    # calculate the progress in percentage
+    percent=$(bc <<< "scale=$bar_percentage_scale; 100 * $current / $total" )
+    # The number of done and todo characters
+    done=$(bc <<< "scale=0; $bar_size * $percent / 100" )
+    todo=$(bc <<< "scale=0; $bar_size - $done" )
+
+    # build the done and todo sub-bars
+    done_sub_bar=$(printf "%${done}s" | tr " " "${bar_char_done}")
+    todo_sub_bar=$(printf "%${todo}s" | tr " " "${bar_char_todo}")
+
+    # output the bar
+    echo -ne "\rProgress : [${done_sub_bar}${todo_sub_bar}] ${percent}%"
+
+    if [ $total -eq $current ]; then
+        echo -e "\nDONE"
+    fi
+}
+
 total=0
 count=0
 min=999
 max=0
 list=""
 echo "Starting test, this may take a bit."
+task_in_total=750
+
 while read output
 do
     ping=$(ping -4 -qc1 $(echo $output | cut -d ";" -f 1) 2>&1 | awk -F'/' 'END{ print (/^rtt/? $5:"FAIL") }')
     list="${list}\n${ping}ms ; ${output}"
-    echo -n "."
     total=$(echo "$total+$ping" | bc)
     count=$(echo "$count+1" | bc)
     if (( $(echo "$ping" != "FAIL" | bc -l) ))
@@ -24,6 +53,7 @@ do
             maxtxt="$output"
         fi
     fi
+show_progress $count $task_in_total
 done < <((curl -s https://raw.githubusercontent.com/dylhost/host-ping-test/refs/heads/main/list.txt| tail -n +4))
 
 echo -e $list | sort -nr
