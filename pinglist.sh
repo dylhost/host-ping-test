6f8629b742502ce30416a59694f840d4957d2137
dylhost <admin@dylhost.tech>
2024-12-11 23:45:53 +1100

    Update pinglist.sh

--- pinglist.sh
+++ pinglist.sh
@@ -49,7 +49,7 @@ task_in_total=$(curl -s https://raw.githubusercontent.com/dylhost/host-ping-test
 while read output
 do
     ping=$(ping -4 -qc1 $(echo $output | cut -d "," -f 1) 2>&1 | awk -F'/' 'END{ print (/^rtt/? $5:"FAIL") }')
-    list="${list}\n${ping}ms,${output}"
+    list="${list}\n${ping}ms, ${output}"
     total=$(echo "$total+$ping" | bc)
     count=$(echo "$count+1" | bc)
     if (( $(echo "$ping" != "FAIL" | bc -l) ))
