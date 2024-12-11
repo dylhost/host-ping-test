3de8f3af3c6ab3eb7997e94d6cd288fd5b0bbef4
dylhost <admin@dylhost.tech>
2024-12-11 19:10:09 +1100

    Update pinglist.sh

--- pinglist.sh
+++ pinglist.sh
@@ -2,11 +2,12 @@ total=0
 count=0
 min=999
 max=0
+list=""
 echo "Starting test, this may take a bit."
 while read output
 do
     ping=$(ping -4 -qc1 $(echo $output | cut -d ";" -f 1) 2>&1 | awk -F'/' 'END{ print (/^rtt/? $5:"FAIL") }')
-    list+="'$ping' ms '$output''\n'"
+    list="${list}\n${ping}ms ; ${output}"
     echo -n "."
     total=$(echo "$total+$ping" | bc)
     count=$(echo "$count+1" | bc)
@@ -23,7 +24,7 @@ do
             maxtxt="$output"
         fi
     fi
-done < <((curl -s https://raw.githubusercontent.com/dylhost/host-ping-test/refs/heads/main/list.txt| tail -n +4))
+done < <((curl -s https://raw.githubusercontent.com/dylhost/host-ping-test/refs/heads/dev/list.txt| tail -n +4))
 
 echo $list | sort -nr
 echo "min/avg/max/total" $min"/"$(echo "$total/$count" | bc)""/""$max"/""$total"
