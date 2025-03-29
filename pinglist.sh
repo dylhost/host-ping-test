44d94e55c95e099c2265acd76e908f7e7a49ec58
dylhost <admin@dylhost.tech>
2025-03-30 01:02:32 +1100

    Update pinglist.sh

--- pinglist.sh
+++ pinglist.sh
@@ -53,8 +53,8 @@ if [ -z ${sortFlag} ]
 fi
 task_in_total=$(curl -s https://raw.githubusercontent.com/dylhost/host-ping-test/refs/heads/main/listtest | awk -F ", " -v ipList=$iplist '$4 == ipList {print $0}' | wc -l)
 
-function ping {
-    ping=$(ping -4 -qc1 $(echo $output | cut -d "," -f 1) 2>&1 | awk -F'/' 'END{ print (/^rtt/? $5:"FAIL") }') 
+function ping() {
+    local ping=$(ping -4 -qc1 $(echo $output | cut -d "," -f 1) 2>&1 | awk -F'/' 'END{ print (/^rtt/? $5:"FAIL") }') 
     list="${list}\n${ping}ms, ${output}"
     total=$(echo "$total+$ping" | bc)
     count=$(echo "$count+1" | bc)
